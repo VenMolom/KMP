1ee2ed199739c5f55267eebae5c39f9f39dbf9a4
VenMolom <venmolom@protonmail.com>
2022-04-23 18:15:44 +0200

    kmp solver

---
+++ KMP/KMPSolver.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KMP
+{
+    public static class KMPSolver
+    {
+        private static List<int> FindPatternPrefixSuffix(string pattern)
+        {
+            var prefixSuffix = new List<int>(pattern.Length + 1);
+            prefixSuffix.Add(0);
+            prefixSuffix.Add(0);
+
+            TextPatternLoop(pattern, pattern, 1, prefixSuffix, (int i, int j) => { prefixSuffix.Add(j); });
+
+            return prefixSuffix;
+        }
+
+        private static ISet<int> FindPatternMatches(string text, string pattern, List<int> prefixSuffix)
+        {
+            var matches = new HashSet<int>();
+
+            TextPatternLoop(text, pattern, 0, prefixSuffix, (int i, int j) =>
+            {
+                if (j == pattern.Length)
+                {
+                    matches.Add(i - pattern.Length + 1);
+                }
+            });
+
+            return matches;
+        }
+
+        private static void TextPatternLoop(string text, string pattern, int start, List<int> prefixSuffix,
+                                               Action<int, int> setInCollection)
+        {
+            int j = 0;
+            for (int i = start; i < text.Length; ++i)
+            {
+                if (j < pattern.Length && text[i] == pattern[j])
+                {
+                    j++;
+                }
+                else
+                {
+                    j = prefixSuffix[j];
+
+                    while (j > 0 && text[i] != pattern[j])
+                    {
+                        j = prefixSuffix[j];
+                    }
+
+                    if (text[i] == pattern[j])
+                    {
+                        j++;
+                    }
+                }
+
+                setInCollection(i, j);
+            }
+        }
+
+        public static ISet<int> FindAllMatches(string text, string pattern)
+        {
+            var prefixSuffix = FindPatternPrefixSuffix(pattern);
+            return FindPatternMatches(text, pattern, prefixSuffix);
+        }
+    }
+}
--- KMP/Program.cs
+++ KMP/Program.cs
@@ -1,9 +1,55 @@
-﻿namespace KMP
+﻿using System;
+using System.IO;
+
+namespace KMP
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                Console.WriteLine($"Wywołanie musi być postaci: " +
+                                  $"{Path.GetFileNameWithoutExtension(Environment.GetCommandLineArgs()[0])} " +
+                                  $"<plik wejściowy> <plik wyjściowy>");
+                return;
+            }
+
+            var inputFilename = args[0];
+            var outputFilename = args[1];
+
+            string text, pattern;
+            try
+            {
+                using (var fr = File.OpenText(inputFilename))
+                {
+                    pattern = fr.ReadLine();
+                    text = fr.ReadLine();
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"Nie można otworzyć pliku {inputFilename}");
+                return;
+            }
+
+            var matches = KMPSolver.FindAllMatches(text, pattern);
+            
+            try
+            {
+                using (var sw = File.CreateText(outputFilename))
+                {
+                    foreach (var match in matches)
+                    {
+                        sw.WriteLine(match);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"Nie można utworzyć pliku {outputFilename}");
+                return;
+            }
         }
     }
 }
